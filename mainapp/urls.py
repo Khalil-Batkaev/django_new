793e4dae139e8e220baf6d61061d15cc338729b5
Khalil Batkaev <xermed@yandex.ru>
2023-09-04 11:43:04 +0300

    [*] Убрал лишний путь

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
 ]
