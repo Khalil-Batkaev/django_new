88e06690943cbca1cf2a8ffec93f439bbc909886
Khalil Batkaev <xermed@yandex.ru>
2023-09-11 10:52:17 +0300

    [+] Базовый шаблон и шаблоны для основных страниц

--- storeapp/views.py
+++ storeapp/views.py
@@ -3,35 +3,9 @@ from django.http import HttpResponse
 
 from storeapp.models import Client, Product, Order
 
-STORE = """
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <a href="/">Главная</a>
-    <a href="#">Магазин</a>
-    <h1>Магазин</h1>
-    <a href="client/">Все клиенты</a><br>
-    </p>
-    <p>
-    <a href="goods/">Все товары</a><br>
-    </p>
-    <p>
-    <a href="order/">Все заказы</a><br>
-    </p>
-    <p>
-  </body>
-</html>
-"""
-
 
 def store(request):
-    return HttpResponse(STORE)
+    return render(request, 'storeapp/store.html', {'title': 'Наш магазин'})
 
 
 def client(request):
