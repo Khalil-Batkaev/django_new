479948967e9947cbaacd6ed82dd3efb1b3eed11c
Khalil Batkaev <xermed@yandex.ru>
2023-09-11 14:03:15 +0300

    [+] Добавил фильтрацию по дате

--- storeapp/views.py
+++ storeapp/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -21,3 +23,23 @@ def product(request):
 def order(request):
     orders = Order.objects.all()
     return render(request, 'storeapp/orders.html', {'title': 'Все заказы', 'orders': orders})
+
+
+def orders_of_client(request, client_id, period=None):
+    DAYS = {
+        'week': 7,
+        'month': 31,
+        'year': 365
+    }
+    if period:
+        pivot = datetime.datetime.now() - datetime.timedelta(DAYS.get(period))
+        orders = Order.objects.filter(client_id=client_id) & Order.objects.filter(created_at__gt=pivot)
+    else:
+        orders = Order.objects.filter(client_id=client_id)
+    return render(request, 'storeapp/client_orders.html', {'title': 'Заказы клиента', 'orders': orders,
+                                                           'period': period})
+
+
+def goods_of_client(request, order_id):
+    goods = Order.objects.filter(pk=order_id).first().product.all()
+    return render(request, 'storeapp/order_detail.html', {'title': 'Товары клиента', 'goods': goods})
